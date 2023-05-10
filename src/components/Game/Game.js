92db4d49e5f65065fc9e3694e24f7d49945a30b1
takna <tak_na@icloud.com>
2023-05-11 08:35:59 +0900

    Exercise 4: Game logic (Completed)

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -8,14 +8,14 @@ import GuessResults from "../GuessResults";
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
+console.log(`Answer: ${answer}`);
 
 function Game() {
   const [guesses, setGuesses] = React.useState([]);
 
   return (
     <>
-      <GuessResults guesses={guesses} />
+      <GuessResults guesses={guesses} answer={answer} />
       <GuessInput guesses={guesses} setGuesses={setGuesses} />
     </>
   );
--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,11 +1,26 @@
 import React from "react";
 import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers";
 
-function Guess({ guess }) {
+function Guess({ guess, answer }) {
+  const resultData = checkGuess(guess, answer);
+  console.log(resultData);
+  // checkGuess() 関数は、以下のようなオブジェクトの配列を返す:
+  // [
+  //   { letter: 'W', status: 'incorrect' },
+  //   { letter: 'H', status: 'incorrect' },
+  //   { letter: 'A', status: 'correct' },
+  //   { letter: 'L', status: 'misplaced' },
+  //   { letter: 'E', status: 'misplaced' },
+  // ]
+
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span key={num} className="cell">
+        <span
+          key={num}
+          className={`cell ${resultData && resultData[num].status}`}
+        >
           {guess && guess[num]}
         </span>
       ))}
