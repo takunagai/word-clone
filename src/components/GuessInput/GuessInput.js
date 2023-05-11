4ca9cee9a5ccee8807abc77b2d435cfb904b207a
takna <tak_na@icloud.com>
2023-05-12 03:31:10 +0900

    Add: Disabled input after the number of tries.

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,12 +1,16 @@
 import { useState } from "react";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+let restOfGuesses = NUM_OF_GUESSES_ALLOWED;
 
 function GuessInput({ guesses, setGuesses }) {
   const [guess, setGuess] = useState("");
+  const [isInputActive, setIsInputActive] = useState(false);
   const handleSubmit = (event) => {
     event.preventDefault();
     if (guess.length === 5) {
       // console.log(guess);
       setGuesses([...guesses, guess]);
+      --restOfGuesses <= 0 && setIsInputActive(true);
       setGuess("");
     } else {
       window.alert("Guess must be 5 characters long.");
@@ -20,6 +24,7 @@ function GuessInput({ guesses, setGuesses }) {
         id="guess-input"
         type="text"
         value={guess}
+        disabled={isInputActive}
         onChange={(event) => {
           setGuess(event.target.value.toUpperCase());
         }}
